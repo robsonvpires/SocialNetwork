4303693260363c63538e9c244e7b0688bd517c21
robsonvpires <robsonvpires@gmail.com>
2020-06-15 22:04:48 -0300

    now users can update, delete and create posts, perhaps a post can only be deleted or updated by its author. Also testing some colors features.

--- users/forms.py
+++ users/forms.py
@@ -6,7 +6,7 @@ from .models import Profile
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
-    
+
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
@@ -24,3 +24,5 @@ class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ['image']
+
+
--- users/views.py
+++ users/views.py
@@ -10,7 +10,7 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}, now you are able to login')
+            messages.success(request, f'Your account has been created! You are now able to log in')
             return redirect('login')
     else:
         form = UserRegisterForm()
@@ -27,15 +27,16 @@ def profile(request):
         if u_form.is_valid() and p_form.is_valid():
             u_form.save()
             p_form.save()
-            messages.success(request, f' Your account has benn updated')
+            messages.success(request, f'Your account has been updated!')
             return redirect('profile')
+
     else:
         u_form = UserUpdateForm(instance=request.user)
         p_form = ProfileUpdateForm(instance=request.user.profile)
 
     context = {
         'u_form': u_form,
-        'p_form': p_form,
+        'p_form': p_form
     }
-    return render(request, 'users/profile.html', context)
 
+    return render(request, 'users/profile.html', context)
